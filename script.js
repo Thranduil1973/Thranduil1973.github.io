5f3eaec5d1ce0a22878c6fae1d3eb76ee316c5f0
Thranduil1973 <bungywungey@gmail.com>
2024-06-19 18:10:30 +0100

    button regeneration

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const myButton = document.getElementById('myButton')
     myButton.addEventListener('click', () => {
         // Change the button text when clicked
-        myButton.textContent = 'You Clicked Me!';
+        myButton.textContent = 'You killed me!';
+        setTimeout(() => {
+            myButton.textContent = `I'm back!`;
+        }, 5000); // 5000 milliseconds = 5 seconds
     });
 })
